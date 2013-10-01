a46d1268243f49637e2738ef5427f806c487d1e1
Uwe Schmitt <uschmitt@mineway.de>
2013-10-01 11:17:44 +0200

    [RELEASE] v 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 
 
-version = (0, 5, 4)
+version = (0, 6, 0)
 
 with open("pyprophet/version.py", "w") as fp:
     fp.write("version = %r" % (version,))
