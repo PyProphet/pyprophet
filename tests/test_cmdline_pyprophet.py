15e0aa034f68ccf264e0169e17a8f4b9e1b812da
Hannes Roest <roest@imsb.biol.ethz.ch>
2014-12-01 20:11:00 +0100

    [TEST] add tests

--- tests/test_cmdline_pyprophet.py
+++ tests/test_cmdline_pyprophet.py
@@ -16,3 +16,20 @@ def test(tmpdir):
             ]:
         full_path = os.path.join(tmpdir.strpath, f)
         assert os.path.exists(full_path)
+
+
+def test_with_probability(tmpdir):
+    here = os.path.dirname(os.path.abspath(__file__))
+    in_file = os.path.join(here, "test_data.txt")
+    _main(["--target.dir=%s" % tmpdir.strpath, "--target.overwrite --compute.probabilities", in_file])
+    for f in ["test_data_summary_stat.csv", "test_data_full_stat.csv", "test_data_report.pdf",
+            "test_data_scorer.bin",
+            "test_data_cutoffs.txt",
+            "test_data_svalues.txt",
+            "test_data_qvalues.txt",
+            "test_data_dscores_top_target_peaks.txt",
+            "test_data_dscores_top_decoy_peaks.txt",
+            ]:
+        full_path = os.path.join(tmpdir.strpath, f)
+        assert os.path.exists(full_path)
+
