944fdd9b8bb0be616499884b0af6a0996ba9c18e
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-07-15 14:45:13 +0200

    SSDM-2049 : extra testing content output files in regression tests

--- tests/test_pyprophet_commandline.py
+++ tests/test_pyprophet_commandline.py
@@ -31,28 +31,78 @@ def _run_cmdline(cmdline):
     return stdout
 
 
-def _run_pyprophet_to_learn_model(tmpdir):
+def _run_pyprophet_to_learn_model(regtest, tmpdir, dump_result_files=False, with_probatilities=False):
     os.chdir(tmpdir.strpath)
     data_path = os.path.join(__here__, "test_data.txt")
     shutil.copy(data_path, tmpdir.strpath)
-    stdout = _run_cmdline("pyprophet test_data.txt --is_test --compute.probabilities")
+    if with_probatilities:
+        stdout = _run_cmdline("pyprophet test_data.txt --is_test --compute.probabilities")
+    else:
+        stdout = _run_cmdline("pyprophet test_data.txt --is_test")
     for f in ["test_data_summary_stat.csv", "test_data_full_stat.csv", "test_data_report.pdf",
               "test_data_scorer.bin",
               "test_data_cutoffs.txt",
               "test_data_svalues.txt",
               "test_data_qvalues.txt",
+              "test_data_with_dscore.csv",
+              "test_data_with_dscore_filtered.csv",
               "test_data_dscores_top_target_peaks.txt",
               "test_data_dscores_top_decoy_peaks.txt",
               ]:
         full_path = os.path.join(tmpdir.strpath, f)
         assert os.path.exists(full_path)
 
+    if not dump_result_files:
+        return stdout
+
+    for f in ["test_data_summary_stat.csv", "test_data_full_stat.csv",
+              "test_data_cutoffs.txt",
+              "test_data_svalues.txt",
+              "test_data_qvalues.txt",
+              "test_data_dscores_top_target_peaks.txt",
+              "test_data_dscores_top_decoy_peaks.txt",
+              "test_data_with_dscore.csv",
+              "test_data_with_dscore_filtered.csv",
+              ]:
+        full_path = os.path.join(tmpdir.strpath, f)
+        lines = open(full_path, "r").readlines()
+        print >> regtest
+        print >> regtest, f, "contains", len(lines), "lines"
+        print >> regtest
+        if len(lines) > 10:
+            print >> regtest, "top 5 lines of", f
+            for line in lines[:5]:
+                if len(line) > 80:
+                    line = line[:40] + " ... " + line[-45:]
+                print >> regtest, line
+            print >> regtest
+            print >> regtest, "last 5 lines of", f
+            for line in lines[-5:]:
+                if len(line) > 80:
+                    line = line[:40] + " ... " + line[-45:]
+                print >> regtest, line
+        else:
+            for line in lines:
+                if len(line) > 80:
+                    line = line[:40] + " ... " + line[-45:]
+                print >> regtest, line
+
     return stdout
+
+
+def test_0(tmpdir, regtest):
+    stdout = _run_pyprophet_to_learn_model(regtest, tmpdir, True, False)
+    _record(stdout, regtest)
+
+
+def test_1(tmpdir, regtest):
+    stdout = _run_pyprophet_to_learn_model(regtest, tmpdir, True, True)
+    _record(stdout, regtest)
 
 
 def test_apply_classifier(tmpdir, regtest):
 
-    stdout = _run_pyprophet_to_learn_model(tmpdir)
+    stdout = _run_pyprophet_to_learn_model(regtest, tmpdir)
 
     _record(stdout, regtest)
 
@@ -92,7 +142,7 @@ def test_apply_classifier(tmpdir, regtest):
 
 def test_apply_weights(tmpdir, regtest):
 
-    stdout = _run_pyprophet_to_learn_model(tmpdir)
+    stdout = _run_pyprophet_to_learn_model(regtest, tmpdir)
     _record(stdout, regtest)
 
     stdout = _run_cmdline("pyprophet test_data.txt --apply_weights=test_data_weights.txt "
