3ca185f873bb6ac8b43448a1ade21b514bb0d43c
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-10 18:00:13 +0200

    SSDM-2425 : release 0.15.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.extension import Extension
 
 import numpy
 
-version = (0, 15, 1)
+version = (0, 15, 2)
 
 ext_modules = [Extension("pyprophet._optimized", ["pyprophet/_optimized.c"])]
 
