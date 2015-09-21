e7f5a35ee32de3f4b3fd094821a7e4f548630358
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-21 22:04:54 +0200

    SSDM-2508 : release 0.17.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.extension import Extension
 
 import numpy
 
-version = (0, 16, 0)
+version = (0, 17, 0)
 
 ext_modules = [Extension("pyprophet._optimized", ["pyprophet/_optimized.c"])]
 
