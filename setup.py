63a17f7f1e83797ffdab8ae2f44dd355cb194cd0
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-10-07 21:50:57 +0200

    release 0.18.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.extension import Extension
 
 import numpy
 
-version = (0, 18, 1)
+version = (0, 18, 2)
 
 ext_modules = [Extension("pyprophet._optimized", ["pyprophet/_optimized.c"])]
 
