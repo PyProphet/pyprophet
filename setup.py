2ddffab2dc242d8f172f3184d89952affc10579b
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2016-03-03 17:23:19 +0100

    updated pandas dependence to 0.17, old version do not work for us

--- setup.py
+++ setup.py
@@ -2,7 +2,18 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 
 
-import numpy
+min_numpy_version = (1, 9, 0)
+
+try:
+    import numpy
+except ImportError:
+    print "need at least numpy %d.%d.%d" % min_numpy_version
+else:
+    vtuple = tuple(map(int, numpy.__version__.split(".")))
+    msg = "need at least numpy %s, found %s" % (min_numpy_version, vtuple)
+    assert vtuple >= min_numpy_version, msg
+
+
 
 
 ######################################################################
@@ -33,16 +44,14 @@ setup(name='pyprophet',
       ],
       zip_safe=False,
       install_requires=[
-          "numpy >= 1.9.0",
-          "pandas >= 0.13",
+          "numpy >= %d.%d.%d" % min_numpy_version,
+          "pandas >= 0.17",
           "scipy >= 0.9.0",
           "numexpr >= 2.1",
           "scikit-learn >= 0.17",
           "matplotlib",
           "seaborn"
       ],
-      test_suite="nose.collector",
-      tests_require="nose",
       entry_points={
           'console_scripts': [
               "pyprophet=pyprophet.main:main",
