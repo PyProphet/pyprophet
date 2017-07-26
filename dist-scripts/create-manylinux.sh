f04660f4088f45a07b21e482072de3dc5963efec
Hannes Roest <hroest@stanford.edu>
2017-07-26 14:45:50 -0700

    [BUILD] update script

--- dist-scripts/create-manylinux.sh
+++ dist-scripts/create-manylinux.sh
@@ -1,29 +1,34 @@
 # Create manylinux packages from current release using the docker image
-
+#
 # based on https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
-
-# sudo docker run --net=host -v `pwd`:/data quay.io/pypa/manylinux1_x86_64 /bin/bash /data/create-manylinux.sh
+#
+# Execute as:
+# 
+#   sudo docker run --net=host -v `pwd`:/data quay.io/pypa/manylinux1_x86_64 /bin/bash /data/create-manylinux.sh
+#
 
 git clone https://github.com/PyProphet/pyprophet.git
 cd pyprophet
+# Apply patch that sets dependency to matplotlib 1.5.3 which does not depend on
+# subprocess32 (which cannot be properly installed under CentOS 5).
+# See https://github.com/matplotlib/matplotlib/issues/8361
+# See https://github.com/google/python-subprocess32/issues/12
 git apply /data/manylinux.patch
 
 # Compile wheels
-for PYBIN in /opt/python/*27*/bin; do
+for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install Cython
   "${PYBIN}/pip" install numpy
   "${PYBIN}/pip" install pandas
-  "${PYBIN}/pip" wheel . -w wheelhouse/
+  "${PYBIN}/pip" wheel . -w wheelhouse_tmp/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/pyprophet*.whl; do
-    auditwheel repair "$whl" -w wheelhouse_final/
+for whl in wheelhouse_tmp/pyprophet*.whl; do
+    auditwheel repair "$whl" -w wheelhouse/
 done
 
 # upload / store result
-mv wheelhouse_final /data/
-## /opt/python/cp27-cp27mu/bin/pip install twine
-## /opt/python/cp27-cp27mu/bin/twine upload wheelhouse_final/* -u hroest
+mv wheelhouse /data/
 
 
