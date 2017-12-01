aa990f1a414cc69984b8051253557829b20ab528
George Rosenberger <gr2578@cumc.columbia.edu>
2017-12-01 16:11:41 -0500

    [FEATURE] IPF optimizations

--- pyprophet/ipf.py
+++ pyprophet/ipf.py
@@ -1,12 +1,5 @@
 # encoding: latin-1
 
-# openblas + multiprocessing crashes for OPENBLAS_NUM_THREADS > 1 !!!
-import os
-os.putenv("OPENBLAS_NUM_THREADS", "1")
-
-import random
-
-
 import pandas as pd
 pd.options.display.width = 220
 pd.options.display.precision = 6
@@ -15,27 +8,31 @@ import numpy as np
 import scipy as sp
 import sqlite3
 
-try:
-    profile
-except NameError:
-    def profile(fun):
-        return fun
-
 from .std_logger import logging
 
-pd.set_option('chained_assignment',None)
+def compute_model_fdr(data):
+    # compute model based FDR estimates from posterior probabilities
+    fdrs = []
+    for t in data:
+        fdrs.append((1-data[data >= t]).sum() / len(data[data >= t]))
+    return np.array(fdrs)
 
 def read_pyp_peakgroup_precursor(path, ipf_max_peakgroup_pep, ipf_ms1_scoring, ipf_ms2_scoring):
+    # precursors are restricted according to ipf_max_peakgroup_pep to exclude very poor peak groups
     con = sqlite3.connect(path)
 
+    # only use MS2 precursors
     if not ipf_ms1_scoring and ipf_ms2_scoring:
-        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, 1 - SCORE_MS2.PEP AS MS2_PEAKGROUP_PP, NULL AS MS1_PRECURSOR_PP, 1 - SCORE_TRANSITION.PEP AS MS2_PRECURSOR_PP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID INNER JOIN (SELECT FEATURE_ID, PEP FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE='' AND TRANSITION.DECOY=0) AS SCORE_TRANSITION ON FEATURE.ID = SCORE_TRANSITION.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, SCORE_MS2.PEP AS MS2_PEAKGROUP_PEP, NULL AS MS1_PRECURSOR_PEP, SCORE_TRANSITION.PEP AS MS2_PRECURSOR_PEP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID INNER JOIN (SELECT FEATURE_ID, PEP FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE='' AND TRANSITION.DECOY=0) AS SCORE_TRANSITION ON FEATURE.ID = SCORE_TRANSITION.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+    # only use MS1 precursors
     elif ipf_ms1_scoring and not ipf_ms2_scoring:
-        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, 1 - SCORE_MS2.PEP AS MS2_PEAKGROUP_PP, 1 - SCORE_MS1.PEP AS MS1_PRECURSOR_PP, NULL AS MS2_PRECURSOR_PP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS1 ON FEATURE.ID = SCORE_MS1.FEATURE_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, SCORE_MS2.PEP AS MS2_PEAKGROUP_PEP, SCORE_MS1.PEP AS MS1_PRECURSOR_PEP, NULL AS MS2_PRECURSOR_PEP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS1 ON FEATURE.ID = SCORE_MS1.FEATURE_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+    # use both MS1 and MS2 precursors
     elif ipf_ms1_scoring and ipf_ms2_scoring:
-        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, 1 - SCORE_MS2.PEP AS MS2_PEAKGROUP_PP, 1 - SCORE_MS1.PEP AS MS1_PRECURSOR_PP, 1 - SCORE_TRANSITION.PEP AS MS2_PRECURSOR_PP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS1 ON FEATURE.ID = SCORE_MS1.FEATURE_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID INNER JOIN (SELECT FEATURE_ID, PEP FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE='' AND TRANSITION.DECOY=0) AS SCORE_TRANSITION ON FEATURE.ID = SCORE_TRANSITION.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, SCORE_MS2.PEP AS MS2_PEAKGROUP_PEP, SCORE_MS1.PEP AS MS1_PRECURSOR_PEP, SCORE_TRANSITION.PEP AS MS2_PRECURSOR_PEP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS1 ON FEATURE.ID = SCORE_MS1.FEATURE_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID INNER JOIN (SELECT FEATURE_ID, PEP FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE='' AND TRANSITION.DECOY=0) AS SCORE_TRANSITION ON FEATURE.ID = SCORE_TRANSITION.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+    # do not use any precursor information
     else:
-        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, 1 - SCORE_MS2.PEP AS MS2_PEAKGROUP_PP, NULL AS MS1_PRECURSOR_PP, NULL AS MS2_PRECURSOR_PP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
+        data = pd.read_sql_query("SELECT FEATURE.ID AS FEATURE_ID, SCORE_MS2.PEP AS MS2_PEAKGROUP_PEP, NULL AS MS1_PRECURSOR_PEP, NULL AS MS2_PRECURSOR_PEP FROM PRECURSOR INNER JOIN FEATURE ON PRECURSOR.ID = FEATURE.PRECURSOR_ID INNER JOIN SCORE_MS2 ON FEATURE.ID = SCORE_MS2.FEATURE_ID WHERE PRECURSOR.DECOY=0 AND SCORE_MS2.PEP < " + str(ipf_max_peakgroup_pep) + ";", con)
 
     data.columns = [col.lower() for col in data.columns]
     con.close()
@@ -43,11 +40,11 @@ def read_pyp_peakgroup_precursor(path, ipf_max_peakgroup_pep, ipf_ms1_scoring, i
     return data
 
 def read_pyp_transition(path, ipf_max_transition_pep, ipf_h0):
-    # Only the evidence is restricted to ipf_max_transition_pep, the peptidoform-space is complete.
+    # only the evidence is restricted to ipf_max_transition_pep, the peptidoform-space is complete
     con = sqlite3.connect(path)
 
     # transition-level evidence
-    evidence = pd.read_sql_query("SELECT FEATURE_ID, TRANSITION_ID, 1 - PEP AS POSTERIOR FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE!='' AND TRANSITION.DECOY=0 AND PEP < " + str(ipf_max_transition_pep) + ";", con)
+    evidence = pd.read_sql_query("SELECT FEATURE_ID, TRANSITION_ID, PEP FROM SCORE_TRANSITION INNER JOIN TRANSITION ON SCORE_TRANSITION.TRANSITION_ID = TRANSITION.ID WHERE TRANSITION.TYPE!='' AND TRANSITION.DECOY=0 AND PEP < " + str(ipf_max_transition_pep) + ";", con)
     evidence.columns = [col.lower() for col in evidence.columns]
 
     # transition-level bitmask
@@ -64,7 +61,7 @@ def read_pyp_transition(path, ipf_max_transition_pep, ipf_h0):
 
     con.close()
 
-    # add h0 to peptidoform-space if necessary
+    # add h0 (peptide_id: -1) to peptidoform-space if necessary
     if ipf_h0:
         peptidoforms = pd.concat([peptidoforms, pd.DataFrame({'feature_id': peptidoforms['feature_id'].unique(), 'peptide_id': -1})])
 
@@ -83,30 +80,29 @@ def prepare_precursor_bm(data):
     data_matrix = []
 
     for i,tr in data.iterrows():
-        if tr['ms1_precursor_pp'] <= 1.0 and tr['ms1_precursor_pp'] >= 0:
-            data_matrix.append({'prior': tr['ms2_peakgroup_pp'], 'evidence': tr['ms1_precursor_pp'], 'hypothesis': True})
-            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pp'], 'evidence': 1-tr['ms1_precursor_pp'], 'hypothesis': False})
-        if tr['ms2_precursor_pp'] <= 1.0 and tr['ms2_precursor_pp'] >= 0:
-            data_matrix.append({'prior': tr['ms2_peakgroup_pp'], 'evidence': tr['ms2_precursor_pp'], 'hypothesis': True})
-            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pp'], 'evidence': 1-tr['ms2_precursor_pp'], 'hypothesis': False})
+        if tr['ms1_precursor_pep'] <= 1.0 and tr['ms1_precursor_pep'] >= 0:
+            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pep'], 'evidence': 1-tr['ms1_precursor_pep'], 'hypothesis': True})
+            data_matrix.append({'prior': tr['ms2_peakgroup_pep'], 'evidence': tr['ms1_precursor_pep'], 'hypothesis': False})
+        if tr['ms2_precursor_pep'] <= 1.0 and tr['ms2_precursor_pep'] >= 0:
+            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pep'], 'evidence': 1-tr['ms2_precursor_pep'], 'hypothesis': True})
+            data_matrix.append({'prior': tr['ms2_peakgroup_pep'], 'evidence': tr['ms2_precursor_pep'], 'hypothesis': False})
 
-    # There is no evidence for a precursor so we set the evidence to 0.
+    # there is no evidence for a precursor so we set the evidence to 0
     if len(data_matrix) == 0:
-            data_matrix.append({'prior': tr['ms2_peakgroup_pp'], 'evidence': 0, 'hypothesis': True})
-            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pp'], 'evidence': 1, 'hypothesis': False})
+            data_matrix.append({'prior': 1-tr['ms2_peakgroup_pep'], 'evidence': 0, 'hypothesis': True})
+            data_matrix.append({'prior': tr['ms2_peakgroup_pep'], 'evidence': 1, 'hypothesis': False})
 
     return pd.DataFrame(data_matrix)
     
 def prepare_transition_bm(data):
-    # peptide_id: -1 indicates h0, i.e. the peak group is wrong!
-
+    # peptide_id = -1 indicates h0, i.e. the peak group is wrong!
     # initialize priors
-    data.ix[data.peptide_id != -1, 'prior'] = data.ix[data.peptide_id != -1, 'precursor_peakgroup_pp'] / data.ix[data.peptide_id != -1, 'num_peptidoforms'] # potential peptidoforms
-    data.ix[data.peptide_id == -1, 'prior'] = 1 - data.ix[data.peptide_id == -1, 'precursor_peakgroup_pp'] # h0
+    data.ix[data.peptide_id != -1, 'prior'] = 1-data.ix[data.peptide_id != -1, 'precursor_peakgroup_pep'] / data.ix[data.peptide_id != -1, 'num_peptidoforms'] # potential peptidoforms
+    data.ix[data.peptide_id == -1, 'prior'] = data.ix[data.peptide_id == -1, 'precursor_peakgroup_pep'] # h0
 
     # set evidence
-    data.ix[data.bmask == 1, 'evidence'] = data.ix[data.bmask == 1, 'posterior'] # we have evidence FOR this peptidoform or h0
-    data.ix[data.bmask == 0, 'evidence'] = 1 - data.ix[data.bmask == 0, 'posterior'] # we have evidence AGAINST this peptidoform or h0
+    data.ix[data.bmask == 1, 'evidence'] = 1-data.ix[data.bmask == 1, 'pep'] # we have evidence FOR this peptidoform or h0
+    data.ix[data.bmask == 0, 'evidence'] = data.ix[data.bmask == 0, 'pep'] # we have evidence AGAINST this peptidoform or h0
 
     data = data[['feature_id','prior','evidence','peptide_id']]
     data = data.rename(columns=lambda x: x.replace('peptide_id', 'hypothesis'))
@@ -114,12 +110,12 @@ def prepare_transition_bm(data):
     return data
 
 def apply_bm(data):
-    # compute likelihood * prior per id & hypothesis
+    # compute likelihood * prior per feature & hypothesis
     # all priors are identical but pandas DF multiplication requires aggregation, so we use min()
     pp_data = (data.groupby(['feature_id',"hypothesis"])["evidence"].prod() * data.groupby(['feature_id',"hypothesis"])["prior"].min()).reset_index()
     pp_data.columns = ['feature_id','hypothesis','likelihood_prior']
 
-    # compute likelihood sum per id
+    # compute likelihood sum per feature
     pp_data['likelihood_sum'] = pp_data.groupby('feature_id')['likelihood_prior'].transform(np.sum)
 
     # compute posterior hypothesis probability
@@ -127,99 +123,90 @@ def apply_bm(data):
 
     return pp_data.fillna(value = 0)
 
-def precursor_inference(data, ipf_ms1_scoring, ipf_ms2_scoring, ipf_max_precursor_pep):
-    # get MS1-level precursor data
+def precursor_inference(data, ipf_ms1_scoring, ipf_ms2_scoring, ipf_max_precursor_pep, ipf_max_precursor_peakgroup_pep):
+    # prepare MS1-level precursor data
     if ipf_ms1_scoring:
-        uis_ms1_precursor_data = data[data['ms1_precursor_pp'] >= 1-ipf_max_precursor_pep][['feature_id','ms1_precursor_pp']].drop_duplicates()
+        ms1_precursor_data = data[data['ms1_precursor_pep'] < ipf_max_precursor_pep][['feature_id','ms1_precursor_pep']].drop_duplicates()
     else:
-        uis_ms1_precursor_data = data[['feature_id']].drop_duplicates()
-        uis_ms1_precursor_data['ms1_precursor_pp'] = np.nan
+        ms1_precursor_data = data[['feature_id']].drop_duplicates()
+        ms1_precursor_data['ms1_precursor_pep'] = np.nan
 
-    # get MS2-level precursor data
+    # prepare MS2-level precursor data
     if ipf_ms2_scoring:
-        uis_ms2_precursor_data = data[data['ms2_precursor_pp'] >= 1-ipf_max_precursor_pep][['feature_id','ms2_precursor_pp']].drop_duplicates()
+        ms2_precursor_data = data[data['ms2_precursor_pep'] < ipf_max_precursor_pep][['feature_id','ms2_precursor_pep']].drop_duplicates()
     else:
-        uis_ms2_precursor_data = data[['feature_id']].drop_duplicates()
-        uis_ms2_precursor_data['ms2_precursor_pp'] = np.nan
+        ms2_precursor_data = data[['feature_id']].drop_duplicates()
+        ms2_precursor_data['ms2_precursor_pep'] = np.nan
 
-    # get MS2-level peak group data
-    uis_ms2_pg_data = data[['feature_id','ms2_peakgroup_pp']].drop_duplicates()
+    # prepare MS2-level peak group data
+    ms2_pg_data = data[['feature_id','ms2_peakgroup_pep']].drop_duplicates()
 
     if ipf_ms1_scoring or ipf_ms2_scoring:
         # merge MS1- & MS2-level precursor and peak group data
-        uis_precursor_data = uis_ms2_precursor_data.merge(uis_ms1_precursor_data, on=['feature_id'], how='outer').merge(uis_ms2_pg_data, on=['feature_id'], how='outer')
+        precursor_data = ms2_precursor_data.merge(ms1_precursor_data, on=['feature_id'], how='outer').merge(ms2_pg_data, on=['feature_id'], how='outer')
 
         # prepare precursor-level Bayesian model
-        uis_precursor_data_bm = uis_precursor_data.groupby('feature_id').apply(prepare_precursor_bm).reset_index()
+        precursor_data_bm = precursor_data.groupby('feature_id').apply(prepare_precursor_bm).reset_index()
 
         # compute posterior precursor probability
-        prec_pp_data = apply_bm(uis_precursor_data_bm)
-        prec_pp_data = prec_pp_data.rename(columns=lambda x: x.replace('posterior', 'precursor_peakgroup_pp'))
+        prec_pp_data = apply_bm(precursor_data_bm)
+        prec_pp_data['precursor_peakgroup_pep'] = 1 - prec_pp_data['posterior']
 
-        inferred_precursors = prec_pp_data[prec_pp_data['hypothesis']][['feature_id','precursor_peakgroup_pp']]
+        inferred_precursors = prec_pp_data[prec_pp_data['hypothesis']][['feature_id','precursor_peakgroup_pep']]
     else:
         # no precursor-level data on MS1 and/or MS2 should be used; use peak group-level data
-        inferred_precursors = uis_ms2_pg_data.rename(columns=lambda x: x.replace('ms2_peakgroup_pp', 'precursor_peakgroup_pp'))
+        inferred_precursors = ms2_pg_data.rename(columns=lambda x: x.replace('ms2_peakgroup_pep', 'precursor_peakgroup_pep'))
+
+    inferred_precursors = inferred_precursors[(inferred_precursors['precursor_peakgroup_pep'] < ipf_max_precursor_peakgroup_pep)]
 
     return inferred_precursors
 
-
-def peptidoform_inference(uis_transition_table, uis_precursor_data):
-    uis_transition_table = pd.merge(uis_transition_table, uis_precursor_data, on='feature_id')
+def peptidoform_inference(transition_table, precursor_data):
+    transition_table = pd.merge(transition_table, precursor_data, on='feature_id')
 
     # compute transition posterior probabilities
-    uis_transition_data_bm = prepare_transition_bm(uis_transition_table)
+    transition_data_bm = prepare_transition_bm(transition_table)
 
     # compute posterior peptidoform probability
-    pp_data = apply_bm(uis_transition_data_bm)
-    pp_data = pp_data.rename(columns=lambda x: x.replace('posterior', 'pf_score'))
+    pf_pp_data = apply_bm(transition_data_bm)
+    pf_pp_data['pep'] = 1 - pf_pp_data['posterior']
 
     # compute model-based FDR
-    pp_data['qvalue'] = compute_model_fdr(pp_data['pf_score'].values)
-    pp_data['pep'] = 1 - pp_data['pf_score']
-    pp_data['PosteriorFullPeptideName'] = pp_data['hypothesis']
+    pf_pp_data['qvalue'] = compute_model_fdr(pf_pp_data['posterior'].values)
 
     # merge precursor-level data with UIS data
-    result = pp_data.merge(uis_precursor_data[['feature_id','precursor_peakgroup_pp']].drop_duplicates(), on=['feature_id'], how='inner')
+    result = pf_pp_data.merge(precursor_data[['feature_id','precursor_peakgroup_pep']].drop_duplicates(), on=['feature_id'], how='inner')
 
     return result
-
-def compute_model_fdr(data):
-    fdrs = []
-    for t in data:
-        fdrs.append((1-data[data >= t]).sum() / len(data[data >= t]))
-    return np.array(fdrs)
 
 def infer_peptidoforms(infile, outfile, ipf_ms1_scoring, ipf_ms2_scoring, ipf_h0, ipf_max_precursor_pep, ipf_max_peakgroup_pep, ipf_max_precursor_peakgroup_pep, ipf_max_transition_pep):
     logging.info("start IPF (Inference of PeptidoForms)")
 
     # precursor level
     logging.info("      prepare precursor-level data")
-    uis_precursor_table = read_pyp_peakgroup_precursor(infile, ipf_max_peakgroup_pep, ipf_ms1_scoring, ipf_ms2_scoring)
+    precursor_table = read_pyp_peakgroup_precursor(infile, ipf_max_peakgroup_pep, ipf_ms1_scoring, ipf_ms2_scoring)
 
     logging.info("      conduct precursor-level inference")
-    uis_precursor_data = precursor_inference(uis_precursor_table, ipf_ms1_scoring, ipf_ms2_scoring, ipf_max_precursor_pep)
-    uis_precursor_data = uis_precursor_data[(uis_precursor_data['precursor_peakgroup_pp'] >= 1-ipf_max_precursor_peakgroup_pep)]
+    precursor_data = precursor_inference(precursor_table, ipf_ms1_scoring, ipf_ms2_scoring, ipf_max_precursor_pep, ipf_max_precursor_peakgroup_pep)
 
     # peptidoform level
     logging.info("      prepare peptidoform-level data")
-    uis_transition_table = read_pyp_transition(infile, ipf_max_transition_pep, ipf_h0)
+    peptidoform_table = read_pyp_transition(infile, ipf_max_transition_pep, ipf_h0)
 
     logging.info("      conduct peptidoform-level inference")
-    uis_peptidoform_data = peptidoform_inference(uis_transition_table, uis_precursor_data)
+    peptidoform_data = peptidoform_inference(peptidoform_table, precursor_data)
 
+    # finalize results and write to table
     logging.info("      finalize results")
-    uis_peptidoform_data = uis_peptidoform_data[uis_peptidoform_data['hypothesis']!=-1][['feature_id','hypothesis','precursor_peakgroup_pp','qvalue','pep']]
-    uis_peptidoform_data.columns = ['FEATURE_ID','PEPTIDE_ID','PRECURSOR_PEAKGROUP_POSTERIOR','QVALUE','PEP']
+    peptidoform_data = peptidoform_data[peptidoform_data['hypothesis']!=-1][['feature_id','hypothesis','precursor_peakgroup_pep','qvalue','pep']]
+    peptidoform_data.columns = ['FEATURE_ID','PEPTIDE_ID','PRECURSOR_PEAKGROUP_PEP','QVALUE','PEP']
 
     if infile != outfile:
         copyfile(infile, outfile)
 
     con = sqlite3.connect(outfile)
 
-    uis_peptidoform_data['PEPTIDE_ID'] = uis_peptidoform_data['PEPTIDE_ID'].astype(int)
-    uis_peptidoform_data.to_sql("SCORE_IPF", con, index=False, if_exists='replace')
+    peptidoform_data.to_sql("SCORE_IPF", con, index=False, if_exists='replace')
     con.close()
 
     logging.info("end IPF")
-
--- tests/test_ipf.py
+++ tests/test_ipf.py
@@ -14,7 +14,7 @@ d.width = 220
 d.precision = 6
 
 def test_0():
-    test_in = {'ms1_precursor_pp': [0.6], 'ms2_peakgroup_pp': [0.8], 'ms2_precursor_pp': [0.5]}
+    test_in = {'ms1_precursor_pep': [0.4], 'ms2_peakgroup_pep': [0.2], 'ms2_precursor_pep': [0.5]}
     tin = pd.DataFrame(data=test_in, index=None)
     test_ref = {'evidence': [0.6, 0.4, 0.5, 0.5], 'hypothesis': [True, False, True, False], 'prior': [0.8, 0.2, 0.8, 0.2]}
     tref = pd.DataFrame(data=test_ref, index=None)
@@ -24,18 +24,6 @@ def test_0():
     assert_frame_equal(tout,tref)
 
 def test_1():
-    test_in = {'peptidoforms': ['PEPA|PEPB','PEPB|PEPC','PEPC'], 'precursor_peakgroup_pp': [0.6,0.6,0.6], 'posterior': [0.1,0.8,0.6]}
-    tin = pd.DataFrame(data=test_in, index=None)
-    test_ref = {'evidence': [0.1, 0.1, 0.9, 0.9, 0.2, 0.8, 0.8, 0.2, 0.4, 0.4, 0.6, 0.4], 'hypothesis': ["PEPA", "PEPB", "PEPC", "h0", "PEPA", "PEPB", "PEPC", "h0", "PEPA", "PEPB", "PEPC", "h0"], 'peptidoforms': ["PEPA|PEPB", "PEPA|PEPB", "PEPA|PEPB", "PEPA|PEPB", "PEPB|PEPC", "PEPB|PEPC", "PEPB|PEPC", "PEPB|PEPC", "PEPC", "PEPC", "PEPC", "PEPC"], 'prior': [0.2, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.4]}
-    tref = pd.DataFrame(data=test_ref, index=None)
-
-    tout = prepare_transition_bm(tin, True)
-    print(tref)
-    print(tout)
-
-    assert_frame_equal(tout,tref)
-
-def test_3():
     tin = np.array([0.5, 0.6, 0.8, 0.9, 0.999, 0.1, 0.3])
     tref = np.array([0.2402000, 0.1752500, 0.1003333, 0.0505000, 0.0010000, 0.4001429, 0.3168333])
 
@@ -43,7 +31,7 @@ def test_3():
 
     assert_almost_equal(tout,tref)
 
-def test_4():
+def test_2():
     test_in = {'feature_id': 'id0','evidence': [0.1, 0.1, 0.9, 0.9, 0.2, 0.8, 0.8, 0.2, 0.4, 0.4, 0.6, 0.4], 'hypothesis': ["PEPA", "PEPB", "PEPC", "h0", "PEPA", "PEPB", "PEPC", "h0", "PEPA", "PEPB", "PEPC", "h0"], 'peptidoforms': ["PEPA|PEPB", "PEPA|PEPB", "PEPA|PEPB", "PEPA|PEPB", "PEPB|PEPC", "PEPB|PEPC", "PEPB|PEPC", "PEPB|PEPC", "PEPC", "PEPC", "PEPC", "PEPC"], 'prior': [0.2, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.4]}
     tin = pd.DataFrame(data=test_in, index=None)
     test_ref = {'feature_id': ['id0','id0','id0','id0'], 'hypothesis': ['PEPA','PEPB','PEPC','h0'],'likelihood_prior': [0.0016, 0.0064, 0.0864, 0.0288],'likelihood_sum': [0.1232, 0.1232, 0.1232, 0.1232],'posterior': [0.012987, 0.051948, 0.701299, 0.233766]}
