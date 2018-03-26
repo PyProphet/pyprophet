9662d25e87d717707edd5d63979ca825943c527e
George Rosenberger <gr2578@cumc.columbia.edu>
2018-03-26 16:00:47 -0400

    [FIX] Formatting

--- pyprophet/data_handling.py
+++ pyprophet/data_handling.py
@@ -16,13 +16,13 @@ except NameError:
 
 # Parameter transformation functions
 def transform_pi0_lambda(ctx, param, value):
-  if value[1] == 0 and value[2] == 0:
-      pi0_lambda = value[0]
-  elif 0 <= value[0] < 1 and value[0] <= value[1] <= 1 and 0 < value[2] < 1:
-      pi0_lambda = np.arange(value[0], value[1], value[2])
-  else:
-      sys.exit('Error: Wrong input values for pi0_lambda. pi0_lambda must be within [0,1).')
-  return(pi0_lambda)
+    if value[1] == 0 and value[2] == 0:
+        pi0_lambda = value[0]
+    elif 0 <= value[0] < 1 and value[0] <= value[1] <= 1 and 0 < value[2] < 1:
+        pi0_lambda = np.arange(value[0], value[1], value[2])
+    else:
+        sys.exit('Error: Wrong input values for pi0_lambda. pi0_lambda must be within [0,1).')
+    return(pi0_lambda)
 
 
 def transform_threads(ctx, param, value):
