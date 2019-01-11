5d0f13e389fb5390101450bf658faa0aed2bf5c8
George Rosenberger <gr2578@cumc.columbia.edu>
2019-01-11 16:36:47 -0500

    [FIX] Regression tests

--- pyprophet/classifiers.py
+++ pyprophet/classifiers.py
@@ -108,7 +108,7 @@ class XGBLearner(AbstractLearner):
                 'scale_pos_weight': "{:.3f}".format(params['scale_pos_weight']),
             }
             
-            clf = xgb.XGBClassifier(silent=1, objective='binary:logitraw', eval_metric='auc', **params)
+            clf = xgb.XGBClassifier(random_state=42, silent=1, objective='binary:logitraw', eval_metric='auc', **params)
 
             score = cross_val_score(clf, X, y, scoring='roc_auc', n_jobs=self.threads, cv=KFold(n_splits=3, shuffle=True, random_state=np.random.RandomState(42))).mean()
             # click.echo("Info: AUC: {:.3f} hyperparameters: {}".format(score, params))
