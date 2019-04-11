b74a5cc6f0e4a40176981c0009695fa45dae4e50
Hannes Roest <hannesroest@gmx.ch>
2019-04-11 15:41:28 -0400

    Update pyprophet/runner.py
    Co-Authored-By: grosenberger <gr2578@cumc.columbia.edu>

--- pyprophet/runner.py
+++ pyprophet/runner.py
@@ -96,7 +96,7 @@ ORDER BY RUN_ID,
 ''', con)
             elif level == "transition":
                 if not check_sqlite_table(con, "SCORE_MS2"):
-                    raise click.ClickException("Transition-level scoring requires prior MS2 or MS1MS2-level scoring.")
+                    raise click.ClickException("Transition-level scoring for IPF requires prior MS2 or MS1MS2-level scoring. Please run 'pyprophet score input.osw' to fix this.")
                 if not check_sqlite_table(con, "FEATURE_TRANSITION"):
                     raise click.ClickException("Transition-level feature table not present in file.")
 
