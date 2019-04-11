191942e4570206b483ece63fd46e72ccc5084037
George Rosenberger <gr2578@cumc.columbia.edu>
2019-04-11 09:03:52 -0400

    [FIX] Click-based exception management

--- pyprophet/main.py
+++ pyprophet/main.py
@@ -234,7 +234,7 @@ def merge(infiles, outfile, same_run, templatefile):
     """
 
     if len(infiles) < 1:
-        sys.exit("Error: At least one PyProphet input file needs to be provided.")
+        raise click.ClickException("At least one PyProphet input file needs to be provided.")
 
     merge_osw(infiles, outfile, templatefile, same_run)
 
