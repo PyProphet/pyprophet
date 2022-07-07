794be1e1e671cbbc09fdf208e10418a30706ec13
George Rosenberger <gr2578@cumc.columbia.edu>
2022-07-07 09:51:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyprophet',
-      version="2.1.10",
+      version="2.1.11",
       author="The PyProphet Developers",
       author_email="rocksportrocker@gmail.com",
       description="PyProphet: Semi-supervised learning and scoring of OpenSWATH results.",
