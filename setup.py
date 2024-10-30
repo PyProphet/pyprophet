ff5804c0effb5cf7da838926faa54a2289b34a6f
Joshua Charkow <joshuacharkow@gmail.com>
2024-10-30 15:04:39 -0400

    convert to .toml setup
    convert to .toml setup for building on github

--- setup.py
+++ setup.py
@@ -1,80 +1,23 @@
 import sys
-from setuptools import setup, find_packages
-from distutils.extension import Extension
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
 
-try:
-    import numpy
-except ImportError:
-    print("Numpy is not installed. Installing it now.")
-    import subprocess
-    subprocess.check_call(['pip', 'install', 'numpy'])
-    import numpy
+use_cython = True
+ext = ".pyx" if use_cython else ".c"
 
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-cmdclass = {}
-ext_modules = []
+extensions = [
+    Extension(
+        "pyprophet._optimized",
+        [f"pyprophet/_optimized{ext}"],
+        include_dirs=[numpy.get_include()],
+    )
+]
 
 if use_cython:
-    ext_modules += [Extension("pyprophet._optimized", ["pyprophet/_optimized.pyx"])]
-    ext_modules = cythonize(ext_modules)
-else:
-    ext_modules += [Extension("pyprophet._optimized", ["pyprophet/_optimized.c"])]
+    extensions = cythonize(extensions)
 
-# read the contents of README for PyPI
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+setup(
+    ext_modules=extensions,
+)
 
-setup(name='pyprophet',
-      version="2.2.8",
-      author="The PyProphet Developers",
-      author_email="rocksportrocker@gmail.com",
-      description="PyProphet: Semi-supervised learning and scoring of OpenSWATH results.",
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      license="BSD",
-      url="https://github.com/PyProphet/pyprophet",
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      include_dirs=[numpy.get_include()],
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Topic :: Scientific/Engineering :: Bio-Informatics',
-          'Topic :: Scientific/Engineering :: Chemistry',
-      ],
-      zip_safe=False,
-      install_requires=[
-          "Click",
-          "duckdb",
-          "numpy >= 1.9.0",
-          "scipy",
-          "pandas >= 0.17",
-          "cython",
-          "numexpr >= 2.10.1",
-          "scikit-learn >= 0.17",
-          "xgboost",
-          "hyperopt",
-          "statsmodels >= 0.8.0",
-          "matplotlib",
-          "tabulate",
-          "pyarrow",
-          "pypdf"
-      ],
-      entry_points={
-          'console_scripts': [
-              "pyprophet=pyprophet.main:cli",
-              ]
-      },
-      ext_modules=ext_modules,
-      )
