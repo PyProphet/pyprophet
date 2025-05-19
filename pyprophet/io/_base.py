804a7d0f81da977e70f2eabf83a65d1045429270
singjc <justincsing@gmail.com>
2025-05-19 12:56:30 -0400

    feat: Add method to write PDF report with scoring results

--- pyprophet/io/_base.py
+++ pyprophet/io/_base.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+import os
 import pickle
+import pandas as pd
 import click
 from .._base import BaseIOConfig
+from ..report import save_report
 
 
 class BaseReader(ABC):
@@ -106,6 +109,89 @@ class BaseWriter(ABC):
             raise NotImplementedError(
                 f"Classifier {self.classifier} not supported for saving weights."
             )
+
+    def _prepare_score_dataframe(
+        self, df: pd.DataFrame, level: str, prefix: str
+    ) -> pd.DataFrame:
+        """
+        Prepare the score DataFrame
+        """
+        score_cols = [
+            "feature_id",
+            "d_score",
+            "peak_group_rank",
+            "p_value",
+            "q_value",
+            "pep",
+        ]
+        if "h_score" in df.columns:
+            score_cols.insert(2, "h_score")
+            score_cols.insert(3, "h0_score")
+
+        if level == "transition":
+            score_cols.insert(1, "transition_id")
+
+        df = df[score_cols].rename(columns=str.upper)
+
+        if level == "transition":
+            key_cols = {"FEATURE_ID", "TRANSITION_ID"}
+        else:
+            key_cols = {"FEATURE_ID"}
+
+        rename_map = {
+            col: f"{prefix}{col}" for col in df.columns if col not in key_cols
+        }
+        return df.rename(columns=rename_map)
+
+    def _get_columns_to_keep(
+        self, existing_cols: list[str], score_prefix: str
+    ) -> list[str]:
+        """
+        Get the columns to keep in the DataFrame by removing existing score columns. Mainly for Parquet files.
+        """
+        drop_cols = [col for col in existing_cols if col.startswith(score_prefix)]
+        if drop_cols:
+            click.echo(
+                click.style(
+                    f"Warn: Dropping existing {score_prefix} columns.", fg="yellow"
+                )
+            )
+        return [col for col in existing_cols if not col.startswith(score_prefix)]
+
+    def _write_pdf_report_if_present(self, result, pi0):
+        """
+        Write a PDF report if the scoring results contain final statistics.
+        """
+
+        if result.final_statistics is None:
+            return
+
+        df = result.scored_tables
+        prefix = self.config.prefix
+        level = self.level
+
+        cutoffs = result.final_statistics["cutoff"].values
+        svalues = result.final_statistics["svalue"].values
+        qvalues = result.final_statistics["qvalue"].values
+
+        pvalues = df[(df.peak_group_rank == 1) & (df.decoy == 0)]["p_value"].values
+        top_targets = df[(df.peak_group_rank == 1) & (df.decoy == 0)]["d_score"].values
+        top_decoys = df[(df.peak_group_rank == 1) & (df.decoy == 1)]["d_score"].values
+
+        pdf_path = os.path.join(prefix + f"_{level}_report.pdf")
+        save_report(
+            pdf_path,
+            self.outfile,
+            top_decoys,
+            top_targets,
+            cutoffs,
+            svalues,
+            qvalues,
+            pvalues,
+            pi0,
+            self.config.runner.color_palette,
+        )
+        click.echo(f"Info: {pdf_path} written.")
 
     def _save_tsv_weights(self, weights):
         """
