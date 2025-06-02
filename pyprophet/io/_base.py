46f8ff1ab2b2b6f62af6385678b8f2ff0437ab9d
singjc <justincsing@gmail.com>
2025-06-02 09:40:50 -0400

    refactor: Update BaseSplitParquetReader to inherit from BaseParquetReader to share similar methods

--- pyprophet/io/_base.py
+++ pyprophet/io/_base.py
@@ -1106,7 +1106,7 @@ class BaseParquetWriter(BaseWriter):
 
 
 @dataclass
-class BaseSplitParquetReader(BaseReader):
+class BaseSplitParquetReader(BaseParquetReader):
     """
     Class for reading and processing data from OpenSWATH results stored in a directoy containing split Parquet files.
 
@@ -1291,90 +1291,9 @@ class BaseSplitParquetReader(BaseReader):
 
         return [c for c in cols if c.startswith(prefix)]
 
-    def _collapse_protein_ids(self, df):
-        """
-        Collapse protein IDs to avoid duplicating feature data in the DataFrame.
-
-        Parameters:
-        - df (DataFrame): Input DataFrame containing feature data.
-
-        Returns:
-        - DataFrame: DataFrame with collapsed protein IDs.
-
-        Example:
-        df = _collapse_protein_ids(df)
-        """
-        org_shape = df.shape
-        # Handle cases where there may be multiple proteins mapping to a peptide with the same feature ID.
-        # We collapse the protein IDs to avoid duplicating feature data
-        df = (
-            df.group_by(["GROUP_ID", "FEATURE_ID"])
-            .agg(
-                [
-                    pl.col("PROTEIN_ID").unique().alias("PROTEIN_ID"),
-                    *[
-                        pl.col(c).first()
-                        for c in df.columns
-                        if c not in {"GROUP_ID", "FEATURE_ID", "PROTEIN_ID"}
-                    ],
-                ]
-            )
-            .sort(["RUN_ID", "PRECURSOR_ID", "EXP_RT"])
-        )
-        collapsed_shape = df.shape
-
-        if org_shape != collapsed_shape:
-            logger.info(
-                f"Collapsed {org_shape[0] - collapsed_shape[0]} of {org_shape[0]} rows due to multiple proteins mapping to the same feature ID."
-            )
-
-        # print(
-        #     df.filter(
-        #         pl.col("PRECURSOR_ID").is_in([7012])
-        #         & pl.col("FEATURE_ID").is_in([33731659783088])
-        #     )
-        # )
-        return df
-
-    def _collapse_ipf_peptide_ids(self, df):
-        """
-        Collapse IPF peptide IDs to avoid duplicating feature data in the DataFrame.
-
-        Parameters:
-        - df (DataFrame): Input DataFrame containing feature data.
-
-        Returns:
-        - DataFrame: DataFrame with collapsed IPF peptide IDs.
-        """
-        org_shape = df.shape
-        # Handle cases where there may be multiple peptides mapping to a precursor with the same feature ID.
-        # We collapse the IPF peptide IDs to avoid duplicating feature data
-        df = (
-            df.group_by(["GROUP_ID", "FEATURE_ID"])
-            .agg(
-                [
-                    pl.col("IPF_PEPTIDE_ID").unique().alias("IPF_PEPTIDE_ID"),
-                    *[
-                        pl.col(c).first()
-                        for c in df.columns
-                        if c not in {"GROUP_ID", "FEATURE_ID", "IPF_PEPTIDE_ID"}
-                    ],
-                ]
-            )
-            .sort(["RUN_ID", "TRANSITION_ID"])
-        )
-        collapsed_shape = df.shape
-
-        if org_shape != collapsed_shape:
-            logger.info(
-                f"Collapsed {org_shape[0] - collapsed_shape[0]} of {org_shape[0]} rows due to multiple transitions features mapping to different peptidoforms."
-            )
-
-        return df
-
 
 @dataclass
-class BaseSplitParquetWriter(BaseWriter):
+class BaseSplitParquetWriter(BaseParquetWriter):
     """
     Class for writing OpenSWATH results to a directory containing split Parquet files.
 
