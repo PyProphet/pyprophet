70f12e2b052c8542ca35b74b5011031dc85ba243
singjc <justincsing@gmail.com>
2025-06-03 19:41:00 -0400

    chore: update subsample_ratio in scoring tests for consistency

--- tests/test_pyprophet_score.py
+++ tests/test_pyprophet_score.py
@@ -524,13 +524,13 @@ class MultiSplitParquetTestStrategy(TestStrategy):
 
     def apply_weights(self):
         # Run scoring without weights to generate initial scores
-        cmd = f"pyprophet score --level ms2 --pi0_method=smoother --pi0_lambda 0.001 0 0 --in={self.input_file} --test --ss_iteration_fdr=0.02 --subsample_ratio=0.8"
+        cmd = f"pyprophet score --level ms2 --pi0_method=smoother --pi0_lambda 0.001 0 0 --in={self.input_file} --test --ss_iteration_fdr=0.02 --subsample_ratio=1.0"
         self.runner.run_command(cmd)
 
         cmd = f"pyprophet score --level ms2 --pi0_method=smoother --pi0_lambda 0.4 0 0 --in={self.input_file} --apply_weights={self.weights_file} --test --ss_iteration_fdr=0.02"
         self.runner.run_command(cmd)
 
-        cmd = f"pyprophet score --level transition --pi0_method=smoother --pi0_lambda 0.001 0 0 --in={self.input_file} --test --ss_iteration_fdr=0.02 --subsample_ratio=0.8"
+        cmd = f"pyprophet score --level transition --pi0_method=smoother --pi0_lambda 0.001 0 0 --in={self.input_file} --test --ss_iteration_fdr=0.02 --subsample_ratio=1.0"
         self.runner.run_command(cmd)
 
         cmd = f"pyprophet score --level transition --pi0_method=smoother --pi0_lambda 0.4 0 0 --in={self.input_file} --apply_weights={self.weights_file} --test --ss_iteration_fdr=0.02"
