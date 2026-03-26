2c36659168da4b05bba672e32eb8edf8451eb3a5
Joshua Charkow <joshuacharkow@gmail.com>
2026-03-26 16:31:08 -0400

    detect version automatically

--- pyprophet/__init__.py
+++ pyprophet/__init__.py
@@ -1,1 +1,10 @@
-__version__='3.0.10'
+from importlib.metadata import PackageNotFoundError, version
+
+def _detect_version() -> str:
+    """Return the installed package version"""
+    try:
+        return version("pyprophet")
+    except PackageNotFoundError:
+        return "0+unknown"
+
+__version__ = _detect_version()
